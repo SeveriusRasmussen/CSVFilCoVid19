e1d7be05d21b6ee1843c36c75d3e74cacec084ca
SeveriusRasmussen <sora0001@stud.kea.dk>
2023-11-05 13:13:34 +0100

    2nd commit?

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        FileHandler fh = new FileHandler();
+
+        // load covid
+        ArrayList<Covid19Data> covid19Datas = fh.loadAllCovid19Data();
+
+        // display covid information
+        for (Covid19Data covid19Data : covid19Datas) {
+            System.out.println(covid19Data);
+        }
     }
 }
